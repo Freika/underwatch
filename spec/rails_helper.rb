98fc8f143f9d199b4a79d321b784bdccb8f20147
Freika <iamfrey@gmail.com>
2016-10-17 06:44:42 +0700

    Init Rails 5.0.

---
+++ spec/rails_helper.rb
@@ -0,0 +1,41 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rspec'
+require 'capybara/poltergeist'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  Capybara.default_driver = :poltergeist
+  Capybara.javascript_driver = :poltergeist
+
+  # Database Cleaner
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
