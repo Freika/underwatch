5f52d0288d048b52124865e23dcb271a3c0512b2
Freika <iamfrey@gmail.com>
2016-10-17 07:48:58 +0700

    Add navbar and test creating event for unauthenticated user.

--- spec/features/events_spec.rb
+++ spec/features/events_spec.rb
@@ -2,18 +2,32 @@ require 'rails_helper'
 
 describe 'Events' do
   let(:event) { build(:event) }
+  let(:user) { create(:user) }
 
   context 'creating' do
-    it 'successful' do
+    it 'successful for user' do
+      login_as user
+
       visit new_event_path
-
-      fill_in 'Title', with: event.title
-      fill_in 'Description', with: event.description
-      select 'Medium', from: 'Difficulty'
-
-      click_on 'Save'
+      fill_form
 
       expect(page).to have_content('Event was successfully created')
     end
+
+    it 'redirect for guest' do
+      visit new_event_path
+
+      expect(page).to have_content('Log in')
+    end
+  end
+
+  private
+
+  def fill_form
+    fill_in 'Title', with: event.title
+    fill_in 'Description', with: event.description
+    select 'Medium', from: 'Difficulty'
+
+    click_on 'Save'
   end
 end
