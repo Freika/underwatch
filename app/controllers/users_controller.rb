bc6ec9d07c62b8e8cfd7759aa30918df72dc06a3
Freika <iamfrey@gmail.com>
2016-11-05 21:12:18 +0700

    Add user profile page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
   def show
+    @user = User.friendly.find(params[:battletag])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
+  get 'users/:battletag', to: 'users#show'
 
   root 'pages#home'
 
